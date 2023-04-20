54f641e3ffb3183797d2fe77a1b99753c68ca3f3
KelvinQiu <kelvinqiu802@outlook.com>
2023-04-20 22:30:34 +0800

    feat: get contact by index api

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -26,6 +26,19 @@ public class Main {
         app.get("/api/contacts", ctx -> {
             ctx.json(contacts);
         });
+
+        app.get("/api/contacts/{id}", ctx -> {
+            try {
+                int index = Integer.parseInt(ctx.pathParam("id"));
+                if (index >= 0 && index < contacts.size()) {
+                    ctx.json(contacts.get(index));
+                } else {
+                    ctx.result("Index is Out of Boundary").status(404);
+                }
+            } catch (NumberFormatException e) {
+                ctx.result("Invalid Index").status(404);
+            }
+        });
     }
 
     public static void initContacts() {
