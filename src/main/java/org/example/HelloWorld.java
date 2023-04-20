0a1a4ff3fecc11e45aed5fd4c0024e63cdffc40f
KelvinQiu <kelvinqiu802@outlook.com>
2023-04-20 21:42:41 +0800

    feat: hello world api

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -5,14 +5,17 @@ import io.javalin.plugin.bundled.CorsPluginConfig;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        var app = Javalin.create(config -> {
-                    config.plugins.enableCors(cors -> {
-                        cors.add(CorsPluginConfig::anyHost);
-                    });
-                })
-                .start(7070);
+        var app = Javalin.create(
+//                config -> {
+//                    config.plugins.enableCors(cors -> {
+//                        cors.add(CorsPluginConfig::anyHost);
+//                    });
+//                }
+        ).start(7070);
 
-        app.get("/", ctx -> ctx.result("Hello Kelvin"));
-        app.get("/name", ctx -> ctx.result("Kelvin"));
+        app.get("/", ctx -> {
+            System.out.println("This API is invoked.");
+            ctx.result("Hello World");
+        });
     }
 }
