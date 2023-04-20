7bb1d4d061de94bd9d17138e33fd010c626de7fa
KelvinQiu <kelvinqiu802@outlook.com>
2023-04-20 22:16:10 +0800

    feat: get all contacts api

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import io.javalin.Javalin;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static List<Contact> contacts = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        initContacts();
+
+        var app = Javalin.create(
+//                config -> {
+//                    config.plugins.enableCors(cors -> {
+//                        cors.add(CorsPluginConfig::anyHost);
+//                    });
+//                }
+        ).start(7070);
+
+        app.get("/", ctx -> {
+            ctx.result("Hello World");
+        });
+
+        app.get("/api/contacts", ctx -> {
+            ctx.json(contacts);
+        });
+    }
+
+    public static void initContacts() {
+        contacts.add(new Contact("Kelvin", "123123123"));
+        contacts.add(new Contact("Skye", "184748393"));
+        contacts.add(new Contact("Jack", "1234283578"));
     }
 }
